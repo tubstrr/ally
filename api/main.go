af0b3c43e60bbab9cfce1fbbafc1ae79e9b96740
tubstrr <me@jonknoll.dev>
2023-08-12 10:13:29 -0500

    fixup! init: Just testing GO :)

--- api/main.go
+++ api/main.go
@@ -1,18 +1,28 @@
-package main
-
-
+// package main
+package handler
+ 
 import (
-    "fmt"
-    "log"
-    "net/http"
+  "fmt"
+  "net/http"
 )
-
-func main() {
-    static := http.FileServer(http.Dir("./static"))
-    http.Handle("/", static)
-
-    fmt.Printf("Starting server at port 443\n")
-    if err := http.ListenAndServe(":443", nil); err != nil {
-        log.Fatal(err)
-    }
+ 
+func Handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
+
+
+// import (
+//     "fmt"
+//     "log"
+//     "net/http"
+// )
+
+// func main() {
+//     static := http.FileServer(http.Dir("./static"))
+//     http.Handle("/", static)
+
+//     fmt.Printf("Starting server at port 443\n")
+//     if err := http.ListenAndServe(":443", nil); err != nil {
+//         log.Fatal(err)
+//     }
+// }
