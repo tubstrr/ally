b597c3c3638459e80980103ffe5062bc14e602f6
tubstrr <me@jonknoll.dev>
2023-08-12 12:48:54 -0500

    update: Using PWD env

--- api/ally-admin.go
+++ api/ally-admin.go
@@ -9,31 +9,33 @@ import (
  
 func AllyAdmin(w http.ResponseWriter, r *http.Request) {
   // fmt.Fprintf(w, "<h1>ally-admin.go</h1>") // write data to response
-
-  backtrack := "../../../../"
-
-
-
-  // currentDir, err := os.Getwd()
+  
+  // backtrack := "../../../../"
+  
+  
+  
+  currentDir := os.Getenv("PWD")
   // if err != nil {
   //   fmt.Println(err)
   // }
-  // fmt.Println(currentDir) // for example /home/user
-
-  filepath := backtrack + "ally/admin/index.html"
-
+  fmt.Println(currentDir) // for example /home/user
+  
+  filepath := os.Getenv("PWD") + "/ally/admin/index.html"
+  
   file, err := os.ReadFile(filepath) // just pass the file name
   if err != nil {
-      fmt.Print(err)
+    fmt.Print(err)
   }
   // fmt.Println(file) // print the content as 'bytes'
   str := string(file) // convert content to a 'string'
   fmt.Fprintf(w, str) // write data to response
-
   // fmt.Println(str) // print the content as a 'string'
 
-  // Directory output helper
-  // dir, err := os.Open(filepath)
+  // // Directory output helper
+  // test:= os.Getenv("PWD")
+  // fmt.Println(test) // for example /home/user
+
+  // dir, err := os.Open(test)
   //  if err != nil {
   //     fmt.Println("Error:", err)
   //     return
@@ -47,5 +49,8 @@ func AllyAdmin(w http.ResponseWriter, r *http.Request) {
   //  for _, file := range files {
   //     fmt.Println(file.Name())
   //  }
+
+   //Get all env variables
+  // fmt.Println(os.Environ())
   
 }
